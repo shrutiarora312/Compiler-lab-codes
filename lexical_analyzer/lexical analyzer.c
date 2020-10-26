3639a54f74437cd7ac4c6802afcdc495518e23f7
Shruti Arora <56230722+shrutiarora312@users.noreply.github.com>
2020-10-26 21:30:00 +0530

    lexical analyzer

--- lexical_analyzer/lexical analyzer.c
+++ lexical_analyzer/lexical analyzer.c
@@ -30,7 +30,7 @@ bool validIdentifier(char* str)
 	if (str[0] == '0' || str[0] == '1' || str[0] == '2' || 
 		str[0] == '3' || str[0] == '4' || str[0] == '5' || 
 		str[0] == '6' || str[0] == '7' || str[0] == '8' || 
-		str[0] == '9' || isDelimiter(str[0]) == true) 
+		str[0] == '9' || isDelimiter(str[0]) == true)
 		return (false); 
 	return (true); 
 } 
